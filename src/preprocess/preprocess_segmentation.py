3b39268ac3e03525648dbfe9bae75d68daa567a2
morgansaras27 <71049091+morgansaras27@users.noreply.github.com>
2020-10-21 10:58:56 -0600

    Update preprocess_segmentation.py
    Updated functions to print status when done running. Updated train/test split algorithm to split based on percentage of dataset. Updated segmentation to not segment across different gesture labels. Added label encoding function for binary vector encoding of labels.

--- src/preprocess/preprocess_segmentation.py
+++ src/preprocess/preprocess_segmentation.py
@@ -2,29 +2,47 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-
 '''SPLITS DATASET INTO TRAINING AND TESTING SETS'''
-def train_test_datasets(df):
+def train_test_datasets(df, percent_train):
     # Differentiate between test set and training set = 96001
-    df_test = df[df['user-id'] > 1000000]
-    df_train = df[df['user-id'] <= 1000000]
+    length = np.shape(df)[0]
+    df_test = df[df['user-id'] > length*percent_train]
+    df_train = df[df['user-id'] <= length*percent_train]
+    print('Train-test dataset split complete.')
+    
     return df_train, df_test
 
 '''NORMALIZE FEATURES'''
 def normalize(dataset):
-    mu = np.mean(dataset, axis=0)
-    sigma = np.std(dataset, axis=0)
-    normalized_dataset = (dataset - mu)/sigma
-    return normalized_dataset
+    pd.options.mode.chained_assignment = None  # default='warn'
+    
+    mu_y = np.mean(dataset['y-axis'], axis=0)
+    mu_z = np.mean(dataset['z-axis'], axis=0)
+    print('\nAverages for y-axis, z-axis = ', mu_y, mu_z)
+    
+    sigma_y = np.std(dataset['y-axis'], axis=0)
+    sigma_z = np.std(dataset['z-axis'], axis=0)
+    print('Std. Deviations = ', sigma_y, sigma_z)
+    
+    normalized_y = (dataset['y-axis'].to_numpy() - mu_y) / sigma_y
+    normalized_z = (dataset['z-axis'].to_numpy() - mu_z) / sigma_z
+    
+    dataset.loc[:,('y-axis')] = normalized_y
+    print('Replaced with normalized y-values.')
+    
+    dataset.loc[:,('z-axis')] = normalized_z
+    print('Replaced with normalized z-values.')
+    dataset.head(5)
+    
+    return dataset
 
 '''CREATES SEGMENTS FROM DATASET'''
 def segmentation(df, window_size, overlap):
-    ''' 
-    INPUTS 
-        dataset = compiled dataset in a pandas dataframe
+    ''' INPUTS: 
+        df = compiled dataset in a pandas dataframe
         window_size = size of segmentation window
-        overlap = number of points of overlap between windows
-    '''
+        overlap = number of points of overlap between window '''
+    
     segments = [] #initialize empty list
     labels = [] #initialize empty list
     num_data_pts = np.shape(df)[0]
@@ -32,11 +50,40 @@ def segmentation(df, window_size, overlap):
     for i in range(0, num_data_pts-window_size, step):
         y_vals = df['y-axis'].values[i: i + window_size]
         z_vals = df['z-axis'].values[i: i + window_size]
-        label = stats.mode(df['activity'][i: i + window_size])[0][0]
-        segments.append([y_vals, z_vals])
-        labels.append(label)
-     # Outputs:list of individualsegments from compiled dataset
+        if (np.size(np.unique(df['activity'][i: i + window_size])[0]) == 1): #1 unique label in segement
+            label = stats.mode(df['activity'][i: i + window_size])[0][0]
+            segments.append([y_vals, z_vals])
+            labels.append(label)
+    
+    print('\nSegmentation complete. ')
+    print('Shape of input dataset: ', np.shape(df))
+    print('Shape of segmented data:', np.shape(segments))
+    print('Shape of labels for segmented data: ', np.shape(labels))
+    
+    
+    ''' OUTPUTS:
+        segments = list of individual segments from compiled dataset
+        labels = list of labels for each segment from compiled dataset '''
     return segments, labels
+
+''' ENCODE LABELS INTO BINARY VECTORS '''
+def label_encode(labels):
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.preprocessing import OneHotEncoder
+    
+    # encode as integers
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(labels)
+    
+    # encode as vectors of 1's and 0's (one-hot)
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    
+    print('\nOnehot encoding complete. Shape of output vectors:', np.shape(onehot_encoded))
+    
+    return onehot_encoded
+
 
 '''PUTTING IT ALL TOGETHER'''
 # Read datafile into dataframe
@@ -46,20 +93,16 @@ data = pd.read_csv(str(f), header = 0) #data = pd.read_csv(str(path) + '\\' + st
 data.head(5)
 
 # Split into test and train datasets
-train_data, test_data = train_test_datasets(data)
-print('Train, test datasets split complete.')
+train_data, test_data = train_test_datasets(data, 0.8)
 
 # Normalize train dataset
 train_data = normalize(train_data)
-print('Normalized training dataset.')
 
 # Create segments
 window_size = 800
 overlap = 100
 time_steps = window_size
 X_train, Y_train = segmentation(train_data, window_size, overlap)
-x_train = x_train.astype("float32")
-y_train = y_train.astype("float32")
-print('Shape of input dataset: ', np.shape(train_data))
-print('Shape of segmented data:', np.shape(segments))
-print('Shape of labels for segmented data: ', np.shape(labels))
+
+# Encode labels as binary vectors
+Y_train_encoded = label_encode(Y_train)
