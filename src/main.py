2cda74ffe24baa2c248a07ac9cb2c38020d04973
morgansaras27 <71049091+morgansaras27@users.noreply.github.com>
2020-11-30 16:09:01 -0700

    Updated main.py with visualization and testing
    Updated main.py with testing and visualization capabilities - loss and accuracy plots, confusion matrices, F1/recall/precision outputs

--- src/main.py
+++ src/main.py
@@ -15,26 +15,30 @@ from tensorflow.keras.optimizers import Adagrad
 #Custom Code Imports
 from preprocess_segmentation import *
 
-NUM_SUBJECTS = 1
+#Initiation of variables for inputs
+NUM_SUBJECTS = 10 #Note: this value will need to be changed if not run on the publicly available dataset
 INPUT_CHANN_COUNT = 2
 WINDOW_SIZE = 800
 OVERLAP = 100
 
+#Initiation of variables for CNN Architecture
 CNN_FILTER_COUNT = 200
 KERNEL_SIZE = 8
 STRIDE_LENGTH = 1
 DROPOUT_RATE = 0.5
 OUTPUT_CLASSES_COUNT = 10
 
+#Initiation of variables for train,test split
 PERCENT_TRAINING = 0.8
 
-def main():
 
+def main():
     #----Load Data----#
     # Read datafile into dataframe
+    #USER NEEDS TO UPDATE THE FILENAME AND PATH/LOCATION IF DIFFERENT 
     f = 'compiled_data.csv' #compiled dataset filename
     path = '../data/' #update with path for locating compiled datset
-    data = pd.read_csv(str(path+f), header = 0) #data = pd.read_csv(str(path) + '\\' + str(f), header = 0)
+    data = pd.read_csv(str(path+f), header = 0)
     data.head(5)
 
     #----Data Preprocessing----#
@@ -59,7 +63,7 @@ def main():
     opt = Adagrad(learning_rate=0.01)
 
     #----Create CNNs----#
-    for cnn_index in range(1): #Only 1 CNN is created for a single subject, currently
+    for cnn_index in range(1, NUM_SUBJECTS, 1): #Only 1 CNN is created for a each subject
         EMG_CNN = Sequential(name="EMG_CNN"+str(cnn_index))
         
         EMG_CNN.add(Reshape((WINDOW_SIZE, INPUT_CHANN_COUNT), input_shape=(1600,)))
@@ -79,13 +83,50 @@ def main():
         print(EMG_CNN.summary())
 
         #----Train CNN----#
-        # print("Training EMG_CNN on data...FILENAME...[%Training Data]")
-        # history = EMG_CNN.fit(X_train[cnn_index], Y_train[cnn_index],batch_size=100, epochs=100,
-        #                  callbacks = [callback])
+        print("Training EMG_CNN on data...FILENAME...[%Training Data]")
+        history = EMG_CNN.fit(X_train[cnn_index], Y_train[cnn_index],batch_size=100, epochs=500, callbacks = [callback], validation_split=0.2, verbose=0)
         
-        #----Run CNN----#
-        print(X_train[0][0].shape)
-        print(EMG_CNN.predict(X_train[0][0:1]))
+	#----Visualization, Testing----#
+        #Visualize training and validation curves, confusion matrix        
+    	print("\n--- Learning curve of model training ---\n")
+    	#Accuracy plot
+    	plt.figure(figsize=(6, 4))
+    	plt.plot(history.history['accuracy'], "g--", label="Accuracy of training data")
+    	plt.plot(history.history['val_accuracy'], "g", label="Accuracy of validation data")
+    	plt.title('Model Accuracy')
+    	plt.ylabel('Accuracy')
+    	plt.xlabel('Training Epoch')
+    	plt.ylim(0)
+    	plt.legend()
+    
+    	#Loss plot
+    	plt.figure(figsize=(6, 4))
+    	plt.plot(history.history['loss'], "r--", label="Loss of training data")
+    	plt.plot(history.history['val_loss'], "r", label="Loss of validation data")
+    	plt.title('Model Loss')
+    	plt.ylabel('Loss')
+    	plt.xlabel('Training Epoch')
+    	plt.ylim(0)
+    	plt.legend()
+    	plt.show()
+
+    	#Evaluate the accuracy and loss value on test data
+    	print("\n--- Check against test data ---\n")
+    	score = EMG_CNN.evaluate(X_test[cnn_index], Y_test[cnn_index], verbose=1)
+    	print("\nAccuracy on test data: %0.2f" % score[1])
+    	print("\nLoss on test data: %0.2f" % score[0])
+
+    	#Display the confusion matrix for test data
+    	print("\n--- Confusion matrix for test data ---\n")
+    	y_pred_test = EMG_CNN.predict(X_test[cnn_index])
+    	# Take the class with the highest probability from the test predictions
+    	max_y_pred_test = np.argmax(y_pred_test, axis=1)
+    	max_y_test = np.argmax(Y_test[cnn_index], axis=1)
+    	show_confusion_matrix(max_y_test, max_y_pred_test)
+ 
+    	#Display classification report (which includes F1, precision, recall) for the test data
+    	print("\n--- Classification report for test data ---\n")
+    	print(classification_report(max_y_test, max_y_pred_test))
         
 
 
